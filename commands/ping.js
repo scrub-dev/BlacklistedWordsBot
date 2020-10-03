fd0fa0aa4533643654660ecf5948defd6132fa5c
Scrub <scott-donaldson@outlook.com>
2020-10-03 14:57:01 +0100

    Added basic Command handling
    Added a simple command handler to next accommodate the ability to update bypasses without needing to restart the bot

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+module.exports = {
+	name: 'ping',
+    description: 'Ping!',
+    args: false,
+	execute(message,args) {
+        const pingEmbed = new Discord.MessageEmbed()
+            .setTitle("Ping:")
+            .setDescription("...Pinging")
+		message.channel.send(pingEmbed).then(m =>{
+            let ping = m.createdTimestamp - message.createdTimestamp
+            const pingEmbed2 = new Discord.MessageEmbed()
+            .setTitle("🏓 Ping")
+            .setDescription(ping + "Ms")
+            m.edit(pingEmbed2)
+        })
+	},
+};
--- index.js
+++ index.js
@@ -11,15 +11,35 @@ client.on('ready', ()=>{
     console.log(`${config.bot.botName} v${config.bot.botVer} running on\n${client.user.username}#${client.user.discriminator} is online!!!`)
 })
 client.on('message', async message =>{
-    let messageArr = message.content.toLowerCase().split(" ")
-    if(messageArr.some(checkMessage)){
-        if(bypassCheck(message)) return
-        let response = responsesFile.responses[Math.floor(Math.random()*responsesFile.responses.length)]
-        message.delete({reason:"Word on blacklist used"})
-            .then(()=>{
-                if(responsesFile.responseFlag) message.channel.send(`${message.member.user.username} ${response}`)
-                else return
-            }).catch(console.error)
+    if(message.author.bot) return;
+    if(!message.content.startsWith(config.prefix)){
+        let messageArr = message.content.toLowerCase().trim().split(/ +/)
+        if(messageArr.some(checkMessage)){
+            if(bypassCheck(message)) return
+            let response = responsesFile.responses[Math.floor(Math.random()*responsesFile.responses.length)]
+            message.delete({reason:"Word on blacklist used"})
+                .then(()=>{
+                    if(responsesFile.responseFlag) message.channel.send(`${message.member.user.username} ${response}`)
+                    else return
+                }).catch(console.error)
+        }
+    }
+
+    let args = message.content.slice(config.prefix.length).trim().split(/ +/);
+    let command = args[0]
+    client.commands = new Discord.Collection();
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+    for(let file of commandFiles){
+        const comand = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    }
+    if(!client.commands.has(command)) return
+    try{
+        client.commands.get(command).execute(message,args)
+    }catch(error)
+    {
+        console.error(error)
+        message.channel.send('An Error has occured: Please check the console for more details')
     }
 })
 function checkMessage(message){
@@ -31,9 +51,14 @@ function checkMessage(message){
 }
 function bypassCheck(message){
     let flag = false
-    const roleCheck = (element) => Array.from(message.member.roles.cache.map(roles => `${roles.id}`)).includes(element)
     if(bypassesFile.channelBypass.includes(message.channel.id)) flag = true
-    if(bypassesFile.roleBypass.some(roleCheck)) flag = true
+    if(bypassesFile.roleBypass.some(element => Array.from(message.member.roles.cache.map(roles => `${roles.id}`)).includes(element))) flag = true
     if(bypassesFile.userBypass.includes(message.member.id))flag = true
     return flag
 }
+
+setInterval(()=>{
+    let activities = ["Trans Rights", "On the belong server"]
+    let num = Math.floor(Math.random() * activities.length)
+    client.user.setActivity(activities[num],{type: "PLAYING"})
+}, 1000 * 60 * 5) //(5 Minutes)
