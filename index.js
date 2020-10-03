f4e175183a14d1ce629718b3193d34387507bc2e
Scrub <scott-donaldson@outlook.com>
2020-10-03 12:57:32 +0100

    Readability and maintainability upgrade
    Move more files and data into json files under Util directory, this is to start with the later process of allowing you to dynamically change values without having to restart the bot because of require caching the files.

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const Discord = require('discord.js')
-const config = require('./config.json')
-const blacklist = require('./blacklist.json')
+const fs = require('fs')
+const config = require('./util/config.json')
+const blacklistFile = require('./util/blacklist.json')
+const bypassesFile = require('./util/bypasses.json')
+const responsesFile = require('./util/responses.json')
 
 let client = new Discord.Client()
 client.login(config.bot.token)
 client.on('ready', ()=>{
     console.log(`${config.bot.botName} v${config.bot.botVer} running on\n${client.user.username}#${client.user.discriminator} is online!!!`)
-    let blackList = ""
-    blacklist.words.forEach(element => {
-        blackList += element + " "
-    })
 })
 client.on('message', async message =>{
     let messageArr = message.content.toLowerCase().split(" ")
     if(messageArr.some(checkMessage)){
         if(bypassCheck(message)) return
-        let responses = ['says Scrub is the best','is telling you tsg needs a neon sign']
-        let response = responses[Math.floor(Math.random()*responses.length)]
+        let response = responsesFile.responses[Math.floor(Math.random()*responsesFile.responses.length)]
         message.delete({reason:"Word on blacklist used"})
             .then(()=>{
-                message.channel.send(`${message.member.user.username} ${response}`)
+                if(responsesFile.responseFlag) message.channel.send(`${message.member.user.username} ${response}`)
+                else return
             }).catch(console.error)
     }
 })
 function checkMessage(message){
     let flag = false
-    blacklist.words.forEach(element => {
+    blacklistFile.words.forEach(element => {
         if(message == element) flag = true
     });
     return flag
@@ -33,8 +32,8 @@ function checkMessage(message){
 function bypassCheck(message){
     let flag = false
     const roleCheck = (element) => Array.from(message.member.roles.cache.map(roles => `${roles.id}`)).includes(element)
-    if(config.bypasses.channelBypass.includes(message.channel.id)) flag = true
-    if(config.bypasses.roleBypass.some(roleCheck)) flag = true
-    if(config.bypasses.userBypass.includes(message.member.id))flag = true
+    if(bypassesFile.channelBypass.includes(message.channel.id)) flag = true
+    if(bypassesFile.roleBypass.some(roleCheck)) flag = true
+    if(bypassesFile.userBypass.includes(message.member.id))flag = true
     return flag
 }
